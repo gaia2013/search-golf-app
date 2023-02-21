9247403cc2d781c816a46d97da0456f7e2696de1
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2023-02-22 00:36:19 +0900

    検索フォームを作成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,55 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>HOME</div>
+  return (
+    <div className="ui container" id="container">
+      <div className="Search__Form">
+        <form className="ui form segment">
+          <div className="field">
+            <label>
+              <i className="calendar alternate outline icon"></i>プレー日
+            </label>
+            <input type="date" />
+          </div>
+          <div className="field">
+            <label>
+              <i className="yen sign icon"></i>上限金額
+            </label>
+            <select name="dropdown" id="" className="ui dropdown">
+              <option value="8000">8000円 </option>
+              <option value="12000">12000</option>
+              <option value="16000">16000</option>
+            </select>
+          </div>
+          <div className="field">
+            <label htmlFor="">
+              <i className="map pin icon"></i>
+              移動時間計算の出発地点（自宅から近い地点をお選びください）
+            </label>
+            <select name="dropdown" id="" className="ui dropdown">
+              <option value="1">東京駅</option>
+              <option value="2">横浜駅</option>
+            </select>
+          </div>
+          <div className="field">
+            <label htmlFor="">
+              <i className="car icon"> </i>車での移動時間の上限
+            </label>
+            <select name="dropdown" id="" className="ui dropdown">
+              <option value="60">60分</option>
+              <option value="90">90分</option>
+              <option value="120">120分</option>
+            </select>
+          </div>
+          <div className="Search__Button">
+            <button type="submit" className="Search__Button__Design">
+              <i className="search icon"> </i>ゴルフ場を検索する
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Home
